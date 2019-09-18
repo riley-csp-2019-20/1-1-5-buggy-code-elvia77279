9ed5a8cd8ae730e5f534ce8a2ff6355ab5405a49
elvia77279 <55498352+elvia77279@users.noreply.github.com>
2019-09-18 10:41:38 -0400

    version 2

--- 115ElviaCastillo.py
+++ 115ElviaCastillo.py
@@ -4,18 +4,20 @@ import turtle as trtl
 spider = trtl.Turtle()
 spider.pensize(40)
 spider.circle(20)
-legs = 4
+legs = 8
 length = 70
 angle = 200 / legs
 spider.pensize(5)
 count = 0
 while (count < legs):
   spider.goto(0,20)
-  spider.setheading(angle*count - 50)
-  spider.forward(length)
-  spider.goto(0,20)
-  spider.setheading(angle*count + 50)
-  spider.forward(length)
+  if count < 4:
+    spider.setheading(angle*count + 0)
+    spider.forward(length)
+  else:
+    spider.setheading(angle*count + 90)
+    spider.forward(length)
+  
   count = count + 1
 spider.hideturtle()
 wn = trtl.Screen()
@@ -25,5 +27,5 @@ wn.mainloop()
 #body of spider
 # y = length
 # z = angle
-# w = limit of number of legs and distance between legs
+# w = legs or limit of number of legs and distance between legs
 # n = count
